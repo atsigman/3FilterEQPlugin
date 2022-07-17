fb11d9e5244ade187801d1ae23d1fd7b1de4b551
Alex Sigman <alex@aiva.ai>
2022-07-17 21:36:24 +0200

    Added sliders to GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,8 +15,19 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+//    setSize (400, 300);
+    
+    // Add and make visible each slider:
+    for ( auto* comp: getComps() )
+    {
+        addAndMakeVisible(comp);
+    }
+    
+    // Embiggen the editor window:
+    setSize(600, 400);
 }
+
+
 
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
@@ -37,4 +48,42 @@ void SimpleEQAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    // Set relative positions for each slider (bounds is stateful, and thus updates after each assignment):
+    
+    auto bounds = getLocalBounds();
+    // response area = 1/3 down from top:
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.33);
+    
+    auto lowCutArea = bounds.removeFromLeft(bounds.getWidth() * 0.33);
+    
+    // Offset to 1/2 of remaining width:
+    auto highCutArea = bounds.removeFromRight(bounds.getWidth() * 0.5);
+    
+    lowCutFreqSlider.setBounds(lowCutArea.removeFromTop(bounds.getHeight() * 0.5));
+    lowCutSlopeSlider.setBounds(lowCutArea);
+    
+    highCutFreqSlider.setBounds(highCutArea.removeFromTop(bounds.getHeight() * 0.5));
+    highCutSlopeSlider.setBounds(highCutArea);
+    
+    peakFreqSlider.setBounds(responseArea);
+    
+    // Offset to 1/2 of remaining height:
+    peakGainSlider.setBounds(bounds.removeFromTop(bounds.getHeight() * 0.5));
+    
+    // Remaining bounds:
+    peakQualitySlider.setBounds(bounds);
+}
+
+std::vector<juce::Component*> SimpleEQAudioProcessorEditor::getComps()
+{
+    return {
+        &peakFreqSlider,
+        &peakGainSlider,
+        &peakQualitySlider,
+        &lowCutFreqSlider,
+        &highCutFreqSlider,
+        &lowCutSlopeSlider,
+        &highCutSlopeSlider
+    };
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,17 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+    
+    CustomRotarySlider(): juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                                       juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +39,19 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+    
+    // The rotary sliders:
+    CustomRotarySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    highCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutSlopeSlider;
+    
+    // Retrieve all sliders as a vector for ease of iteration through all sliders:
+    std::vector<juce::Component*> getComps();
+    
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
