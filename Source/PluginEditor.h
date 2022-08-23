ba2471a044e3f2d81be69f83a8098c2d0e5be64a
Alex Sigman <alex@aiva.ai>
2022-08-23 23:25:09 +0200

    Customised bypass button shape

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -166,6 +166,11 @@ struct LookAndFeel : juce::LookAndFeel_V4
                           float rotaryStartAngle,
                           float rotaryEndAngle,
                          juce::Slider&) override;
+    
+    void drawToggleButton(juce::Graphics &g,
+                          juce::ToggleButton &toggleButton,
+                          bool shouldDrawButtonAsHighlighted,
+                          bool shouldDrawButtonAsDown) override;
 };
 
 // Rotary Slider customisation:
@@ -343,6 +348,9 @@ private:
     
     // Retrieve all sliders as a vector for ease of iteration through all sliders:
     std::vector<juce::Component*> getComps();
+    
+    // LookAndFeel instance: 
+    LookAndFeel lnf;
     
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
